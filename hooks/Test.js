5fd55b37a4a01603556a403bd5733b4a446b0615
Andrés González <andres@atenea.ai>
2023-10-06 17:38:54 -0500

    First commit.

---
+++ hooks/Test.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const useTest(initialValue = false) {
+  const [value, setValue] = useState(initialValue);
+
+  const toggle = () => {
+    setValue((prevValue) => !prevValue);
+  };
+
+  return [value, toggle];
+}
+
+export default useTest;
